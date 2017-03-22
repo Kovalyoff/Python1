4e3c9d644da0e252700fb6fc44be9ff974a88ab3
Kovalyoff <vitaliy.kovalyoff@gmail.com>
2017-03-22 18:48:40 +0200

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,2 +1,2 @@
-scp /var/lib/jenkins/workspace/Python/deploy/hello.py user@172.31.106.83:/var/www/demoapp/
+scp -i /home/user/.ssh/id_rsa /var/lib/jenkins/workspace/Python/deploy/hello.py user@172.31.106.83:/var/www/demoapp/
 ssh -i /home/user/.ssh/id_rsa user@172.31.106.83 'sudo service uwsgi restart'
