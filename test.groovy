a69d142a349f2d90cdceae80d4079beaee2333fe
Kovalyoff <vitaliy.kovalyoff@gmail.com>
2017-03-22 18:29:34 +0200

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,1 +1,1 @@
-pylint '/var/lib/jenkins/jobs/Python1/build/workspace/hello.py' || cp '/var/lib/jenkins/jobs/Python1/test/workspace/hello.py'  /var/lib/jenkins/jobs/Python1/build/workspace/
+pylint '/var/lib/jenkins/workspace/Python/test/hello.py' || cp '/var/lib/jenkins/workspace/Python/test/hello.py'  /var/lib/jenkins/workspace/Python/build
